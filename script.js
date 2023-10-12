d25885ce90fd80291aad68e65859bb3c0ab93eed
Ange230700 <kouakouangeericstephane@gmail.com>
2023-10-12 21:40:40 +0200

    Using every function part 2

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-const animals = [
-    { name: 'Waffles', species: 'dog' },
-    { name: 'Fluffy', species: 'cat' },
-    { name: 'Spelunky', species: 'dog' },
-    { name: 'Hank', species: 'cat' },
-    { name: 'Mia', species: 'dog' },
-    { name: 'Bella', species: 'cat' },
-    { name: 'Chloe', species: 'dog' },
-    { name: 'Daisy', species: 'dog' },
-    { name: 'Luna', species: 'cat' }
+const teamOne = [
+    { name: 'Waffles', age: '19' },
+    { name: 'Fluffy', age: '14' },
+    { name: 'Spelunky', age: '4' },
+    { name: 'Hank', age: '11'  },
+    { name: 'Mia', age: '34'  },
+    { name: 'Bella', age: '8'  },
+    { name: 'Chloe', age: '13'  },
+    { name: 'Daisy', age: '4'  },
+    { name: 'Luna', age: '26'  },
 ];
 
-const animalList = document.querySelector('.animal-list');
+const teamTwo = [
+    { name: 'Goldie', age: '39' },
+    { name: 'Biscuit', age: '25' },
+    { name: 'Penny', age: '79' },
+    { name: 'Dexter', age: '69' },
+    { name: 'Finn', age: '76' },
+    { name: 'Duke', age: '59' },
+    { name: 'Lucky', age: '19' },
+    { name: 'Sam', age: '18' },
+    { name: 'Teddy', age: '39' },
+];
 
-const animalListFiltered = animals.filter(
-    animal => animal.species === 'cat' 
-);
+function checkIfAdult(array) {
+    return array.every((person) => person.age >= 18);
+}
 
-animalList.innerHTML = animalListFiltered.map(
-    animal => `<li>${animal.name} >> ${animal.species}</li>`
-).join('');
+console.log(checkIfAdult(teamOne));
+console.log(checkIfAdult(teamTwo));
